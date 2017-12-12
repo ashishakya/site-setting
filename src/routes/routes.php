3126cc564cfe3b7bdfb50628439d6ae7e8daddd8
Babish Shrestha <babishshrestha@gmail.com>
2017-12-12 11:41:44 +0545

    Config key fix in routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,9 +1,9 @@
 <?php
 $groupParameters['namespace'] = 'Proshore\SiteSetting\Http\Controllers';
-if (config('menumanagement.prefix')) {
+if (config('proshore-menu-management.prefix')) {
     $groupParameters['prefix'] = config('proshore-site-setting.prefix');
 }
-if (config('menumanagement.middleware')) {
+if (config('proshore-menu-management.middleware')) {
     $groupParameters['middleware'] = config('proshore-site-setting.middleware');
 }
 Route::group($groupParameters, function () {
