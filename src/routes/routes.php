90a23d73737289bfef1c5e2e7ad443d97f60bc2b
Babish Shrestha <babishshrestha@gmail.com>
2017-12-12 15:15:46 +0545

    Update routes.php

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 $groupParameters['namespace'] = 'Proshore\SiteSetting\Http\Controllers';
-if (config('proshore-menu-management.prefix')) {
+if (config('proshore-site-setting.prefix')) {
     $groupParameters['prefix'] = config('proshore-site-setting.prefix');
 }
-if (config('proshore-menu-management.middleware')) {
+if (config('proshore-site-setting.middleware')) {
     $groupParameters['middleware'] = config('proshore-site-setting.middleware');
 }
 Route::group($groupParameters, function () {
